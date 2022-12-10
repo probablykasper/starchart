6a55105a9dbad9fff37b3d6f0cfc8dc656e82e9b
Kasper <kasperkh.kh@gmail.com>
2022-12-10 23:40:05 +0100

    SvelteKit template

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Starchart
+
+## Commands
+
+- `npm run dev`: Start in dev mode
+- `npm run build`: Build
+- `npm run preview`: Preview production app
+- `npm run lint`: Lint
+- `npm run format`: Format
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	let count = 0
+	onMount(() => {
+		const interval = setInterval(() => count++, 1000)
+		return () => clearInterval(interval)
+	})
+</script>
+
+<h1>Starchart</h1>
+
+<style lang="sass">
+  :global(body)
+    margin: 100px 0px
+    font-family: Arial, Helvetica, sans-serif
+    font-size: 18px
+    background-color: #111318
+    color: #f2f2f2
+    text-align: center
+  h1
+    color: #ffdd00
+</style>
