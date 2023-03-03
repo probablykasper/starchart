4a44ee1f843ff7dd553fb9a976bda517b1b6a7d2
Kasper <kasperkh.kh@gmail.com>
2023-03-03 13:05:50 +0100

    Add more colors

--- src/routes/color.ts
+++ src/routes/color.ts
@@ -1,13 +1,27 @@
 import { TinyColor } from '@ctrl/tinycolor'
 
-export const hexColors = ['#E91E63', '#0064fa', '#ffdd00', '#FF9800', '#08fd96', '#FFFFFF']
-export const topColors = hexColors.map((hexColor) => {
+export const colors = [
+	'hsl(340, 100%, 52%)',
+	'hsl(216, 100%, 49%)',
+	'hsl(36, 100%, 50%)',
+	'hsl(52, 100%, 50%)',
+	'hsl(155, 100%, 45%)',
+	'hsl(257, 100%, 61%)',
+	'hsl(188, 90%, 52%)',
+	'hsl(306, 100%, 52%)',
+	'hsl(0, 0%, 100%)',
+]
+export const hexColors = colors.map((hexColor) => {
+	const color = new TinyColor(hexColor)
+	return color.brighten(10).toHexString()
+})
+export const topColors = colors.map((hexColor) => {
 	const color = new TinyColor(hexColor)
 	color.setAlpha(0.5)
-	return color.saturate(100).toRgbString()
+	return color.saturate(100).toHslString()
 })
-export const bottomColors = hexColors.map((hexColor) => {
+export const bottomColors = colors.map((hexColor) => {
 	const color = new TinyColor(hexColor)
 	color.setAlpha(0)
-	return color.darken(0).toRgbString()
+	return color.darken(0).toHslString()
 })
