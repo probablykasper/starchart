54eeda90c64fb9c92f25aeca377d2084a16f7add
Kasper <kasperkh.kh@gmail.com>
2025-04-24 06:59:32 +0200

    Fix empty page not being plank

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -149,7 +149,9 @@
 		<ChartComponent bind:chart {screenshot_mode} />
 	{/if}
 	<div class="chart" bind:clientWidth={width}>
-		<div class="watermark">starchart.kasper.space</div>
+		{#if chart && $chart && $chart.lines.length > 0}
+			<div class="watermark">starchart.kasper.space</div>
+		{/if}
 		<div bind:this={container} class:hidden={!$chart || $chart.lines.length === 0}></div>
 		{#if chart && $chart && $chart.lines.length > 0}
 			<Tooltip {chart} />
@@ -189,29 +191,29 @@
 			/>
 			Logarithmic
 		</label>
-	{/if}
-	<button
-		type="button"
-		class="bordered button download-button"
-		on:click={download_image_of_element}
-	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="16"
-			height="16"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="lucide lucide-download-icon lucide-download"
-			><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline
-				points="7 10 12 15 17 10"
-			/><line x1="12" x2="12" y1="15" y2="3" /></svg
+		<button
+			type="button"
+			class="bordered button download-button"
+			on:click={download_image_of_element}
 		>
-		Download image
-	</button>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="lucide lucide-download-icon lucide-download"
+				><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline
+					points="7 10 12 15 17 10"
+				/><line x1="12" x2="12" y1="15" y2="3" /></svg
+			>
+			Download image
+		</button>
+	{/if}
 </div>
 
 <style lang="sass">
