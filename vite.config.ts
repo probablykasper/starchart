4ab3c4d246c66251396829498aac3b401abc5580
Kasper <kasperkh.kh@gmail.com>
2026-01-29 13:20:28 +0100

    Update octokit

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,4 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			// octokit fix
-			'node-fetch': 'isomorphic-fetch',
-		},
-	},
 })
